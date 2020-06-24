1e6607287cbab92b1c8f35e98848b8761619eb6a
Annelies <0962949@hr.nl>
2020-06-24 20:12:30 +0200

    different messages

--- dev/captain.ts
+++ dev/captain.ts
@@ -21,7 +21,7 @@ class Captain extends HTMLElement{
     public onCollision(numberOfHits : number) {
         if(numberOfHits == 1)  { 
             this.style.backgroundImage = `url(images/emote_alert.png)`
-            console.log(`Captain of ${this.ship.color} pirateship WOKE UP!`)
+            console.log(`Captain of ${this.ship.color} pirateship WOKE UP!`) 
             Messageboard.getInstance().addMessage(`Captain of ${this.ship.color} pirateship WOKE UP!`)
         }
         else if(numberOfHits == 7) {
